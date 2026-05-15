348ca0da32ecdba38a9858f582af84f591d1cbf4
marcel <marcelewane@yahoo.fr>
2026-05-15 09:25:06 +0200

    Added lombok annotations and remove constructor and toString in Item.java

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,5 +1,12 @@
 package com.gildedrose;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
     public String name;
@@ -8,14 +15,4 @@ public class Item {
 
     public int quality;
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
 }
