97f890e367eb3569932c8d5c062abea293fe2412
marcel <marcelewane@yahoo.fr>
2026-05-15 11:22:16 +0200

    change updateQuality method implementation in GildedRose.java

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,86 @@
 package com.gildedrose;
 
-class GildedRose {
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class GildedRose {
+
     Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
-
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+        for (Item item : items) {
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            if(isSulfuras(item))
+                continue;
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            updateQualityValue(item);
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            item.sellIn--;
+
+            if(item.sellIn < 0)
+                updateExpiredItem(item);
         }
     }
+
+    // ======================================
+    // UPDATE BEFORE EXPIRATION
+    // ======================================
+    private void updateQualityValue(Item item) {
+        if(isAgedBrie(item))
+            increaseQuality(item,1);
+        else if(isBackstagePass(item)) {
+            if (item.sellIn <= 5)
+                increaseQuality(item, 3);
+            else if (item.sellIn <= 10)
+                increaseQuality(item, 2);
+            else
+                increaseQuality(item, 1);
+        }
+        else if(isConjured(item))
+            decreaseQuality(item, 2);
+        else
+            decreaseQuality(item, 1);
+    }
+
+    // ======================================
+    // UPDATE AFTER EXPIRATION
+    // ======================================
+    private void updateExpiredItem(Item item) {
+
+        if (isAgedBrie(item)) //aged brie product
+            increaseQuality(item, 1);
+        else if (isBackstagePass(item)) //backstage product
+            item.quality = 0;
+        else if (isConjured(item)) // conjured product
+            decreaseQuality(item, 2);
+        else // normal product
+            decreaseQuality(item, 1);
+    }
+
+    private boolean isConjured(Item item) {
+        return "Conjured Mana Cake".equals(item.getName());
+    }
+
+    private void decreaseQuality(Item item, int amount) {
+        item.setQuality(Math.max(0, item.quality - amount));
+    }
+
+    private void increaseQuality(Item item, int amount) {
+        item.setQuality(Math.min(50, item.quality + amount));
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return "Backstage passes to a TAFKAL80ETC concert".equals(item.getName());
+    }
+
+    private boolean isAgedBrie(Item item) {
+        return "Aged Brie".equals(item.getName());
+    }
+
+
+    private boolean isSulfuras(Item item) {
+        return "Sulfuras, Hand of Ragnaros".equals(item.getName());
+    }
+
 }
